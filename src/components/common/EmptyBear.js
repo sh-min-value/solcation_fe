c3a049cf8cd962e001a7a0f4e55423948dfad7eb
hwang-hyesung <hwanghs7008@g.skku.edu>
2025-09-18 21:31:40 +0900

    Fix: 곰돌이 수정

--- src/components/common/EmptyBear.js
+++ src/components/common/EmptyBear.js
@@ -3,9 +3,9 @@ import bear from '../../assets/images/empty_sol.svg';
 
 const EmptyBear = ({ title, description, onClick, buttonText }) => {
     return (
-        <div className="p-4">
-            <div className="text-center py-12">
-                <img src={bear} alt="plan" className="w-60 h-60 mx-auto mb-4" />
+        <div className="p-4 h-full flex flex-col justify-center">
+            <div className="text-center">
+                <img src={bear} alt="plan" className="h-48 w-auto mx-auto mb-4" />
                 <h3 className="text-lg font-semibold text-gray-700">{title}</h3>
                 <p className="text-gray-500 text-sm mb-6">
                     {description}
