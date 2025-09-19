fdc68f65baa000fbbf4752e18b3669d6f1b17c2d
hwang-hyesung <hwanghs7008@g.skku.edu>
2025-09-19 11:04:23 +0900

    Fix: 여행 테마 태그 수정

--- src/utils/CategoryIcons.js
+++ src/utils/CategoryIcons.js
@@ -70,56 +70,56 @@ export const getTravelCategoryIcon = (
       return (
         <>
           <BiSolidBowlRice className={className} />
-          <p className='w-18 truncate'>음식, 미식</p>
+          <p className='max-w-16 truncate'>음식, 미식</p>
         </>
       );
     case 'LEISURE':
       return (
         <>
           <MdKitesurfing className={className} />
-          <p className='w-18 truncate'>레저, 액티비티</p>
+          <p className='max-w-16 truncate'>레저, 액티비티</p>
         </>
       );
     case 'RECREATION':
       return (
         <>
           <FaTree className={className} />
-          <p className='w-18 truncate'>휴양, 힐링</p>
+          <p className='max-w-16 truncate'>휴양, 힐링</p>
         </>
       );
     case 'CULTURE':
       return (
         <>
           <HiLibrary className={className} />
-          <p className='w-18 truncate'>문화, 역사</p>
+          <p className='max-w-16 truncate'>문화, 역사</p>
         </>
       );
     case 'SHOPPING':
       return (
         <>
           <AiFillShopping className={className} />
-          <p className='w-18 truncate'>쇼핑, SNS 핫플레이스</p>
+          <p className='max-w-16 truncate'>쇼핑, SNS 핫플레이스</p>
         </>
       );
     case 'FESTIVAL':
       return (
         <>
           <MdFestival className={className} />
-          <p className='w-18 truncate'>시즌 축제</p>
+          <p className='max-w-16 truncate'>시즌 축제</p>
         </>
       );
     case 'SIGHTSEEING':
       return (
         <>
           <BiSolidBus className={className} />
-          <p className='w-18 truncate'>관광</p>
+          <p className='max-w-16 truncate'>관광</p>
         </>
       );
     default:
       return (
         <>
           <BiDotsHorizontalRounded className={className} />
-          <p className='w-18 truncate'>기타</p>
+          <p className='max-w-16 truncate'>기타</p>
         </>
       );
   }
