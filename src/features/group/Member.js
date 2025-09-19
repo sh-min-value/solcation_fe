aeb3a7a0c81261189d6e32d86abe327280b06e10
subin930 <bread0930@g.skku.edu>
2025-09-20 00:08:24 +0900

    Fix: 그룹 멤버 UI 수정, 스크롤 적용

--- src/features/group/Member.js
+++ src/features/group/Member.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { EmojiProvider, Emoji } from 'react-apple-emojis';
 import emojiData from 'react-apple-emojis/src/data.json';
+import Badge from './Badge';
 
 const Member = ({
   userName,
@@ -8,13 +9,14 @@ const Member = ({
   showIcon = false,
   icon: IconComponent,
   className = '',
+  type = 'MEMBER',
 }) => {
   return (
     <div
-      className={`bg-white backdrop-blur-sm rounded-3xl p-2 shadow-[0_0_10px_rgba(0,0,0,0.1)] hover:shadow-[0_0_20px_rgba(0,0,0,0.15)] transition-all duration-200 cursor-pointer transform hover:-translate-y-1 h-16 flex flex-row items-center space-x-4 ${className}`}
+      className={`bg-white backdrop-blur-sm rounded-3xl p-1 shadow-[0_0_5px_rgba(0,0,0,0.05)] transition-all duration-200 cursor-pointer transform hover:-translate-y-1 h-16 flex flex-row items-center space-x-4 ${className}`}
     >
       {/* 프로필 사진 */}
-      <div className="bg-light-blue w-11 h-11 rounded-full ml-3 shadow-[0_0_10px_rgba(0,0,0,0.2)] flex items-center justify-center">
+      <div className="bg-light-blue w-11 h-11 rounded-full ml-3 shadow-[0_0_5px_rgba(0,0,0,0.1)] flex items-center justify-center">
         <EmojiProvider data={emojiData}>
           <Emoji name="spiral-shell" size={10} className="w-8 h-8"></Emoji>
         </EmojiProvider>
@@ -26,14 +28,16 @@ const Member = ({
           <h3 className="text-xl font-bold text-third truncate max-w-[5.5rem]">
             {userName}
           </h3>
-          <span className="text-third">|</span>
+          {/* <span className="text-third/50 font-thin">|</span> */}
           {/* 아이콘 표시 */}
-          {showIcon && IconComponent && (
+          {/* {showIcon && IconComponent && (
             <div className="ml-2">
               <IconComponent size={15} className="inline-block text-group-1" />
             </div>
           )}
-          <span className="text-medium text-third">{userRole}</span>
+          <span className="text-medium text-third">{userRole}</span> */}
+          <Badge type={type} />
+          {type === 'LEADER' && <Badge type={'MEMBER'} />}
         </div>
       </div>
     </div>
