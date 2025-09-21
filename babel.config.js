1d6b7d9cd454198183af76071bb8eb3ff9496e55
hwang-hyesung <hwanghs7008@g.skku.edu>
2025-09-21 18:23:22 +0900

    fix: Jest 설정 개선 및 Babel 설정 업데이트

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,37 @@
 module.exports = {
-    presets: [
+  presets: [
+    [
       '@babel/preset-env',
-      '@babel/preset-react',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
     ],
-  };
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+      },
+    ],
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+  ],
+  env: {
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: 'current',
+            },
+          },
+        ],
+        '@babel/preset-react',
+      ],
+    },
+  },
+};
---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleNameMapping: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-router-dom)/)',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/index.js',
+    '!src/reportWebVitals.js',
+    '!src/setupTests.js',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+};
